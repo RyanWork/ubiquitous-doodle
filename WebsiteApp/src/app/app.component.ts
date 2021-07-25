43cfade5413015d84c273e83a52536d691939872
Ryan Ha <ryanha.mail@gmail.com>
2021-07-24 23:20:22 -0400

    Expose observables for binding submit button

--- WebsiteApp/src/app/app.component.ts
+++ WebsiteApp/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnDestroy} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { EmailService } from "./services/email.service";
-import { Subject } from "rxjs";
-import { takeUntil } from "rxjs/operators";
+import {BehaviorSubject, Observable, Subject} from "rxjs";
+import {finalize, takeUntil, tap} from "rxjs/operators";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { MatSnackBar } from "@angular/material/snack-bar";
 
@@ -11,6 +11,12 @@ import { MatSnackBar } from "@angular/material/snack-bar";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnDestroy {
+
+  sending$: Observable<boolean>;
+
+  private invalid: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+  invalid$ = this.invalid.asObservable();
+
   title = 'Ryan Ha';
 
   emailForm = new FormGroup({
@@ -24,6 +30,10 @@ export class AppComponent implements OnDestroy {
   private destroy$: Subject<void> = new Subject<void>()
 
   constructor(private emailService: EmailService, private snackBar: MatSnackBar) {
+    this.sending$ = emailService.sending$;
+    this.emailForm.statusChanges
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(result => this.invalid.next(result == 'INVALID'));
   }
 
   sendEmail() {
@@ -32,13 +42,15 @@ export class AppComponent implements OnDestroy {
       emailBody: this.emailForm.controls['emailBody']?.value
     })
       .pipe(takeUntil(this.destroy$))
-      .subscribe((result) => {
+      .subscribe(() => {
         this.snackBar.open('Email sent!');
-        console.log(result);
-      }, error => {
-        this.snackBar.open(`Error: ${error}`);
-        console.log(error);
+      }, () => {
+        this.snackBar.open('Something went wrong, please email me@ryanha.dev');
       })
+  }
+
+  formInvalid() {
+
   }
 
   ngOnDestroy(): void {
--- WebsiteApp/src/app/services/email.service.ts
+++ WebsiteApp/src/app/services/email.service.ts
@@ -1,16 +1,23 @@
 import { Injectable} from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Email } from "../../model/Email";
+import {Observable, Subject} from "rxjs";
+import {tap} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
 
+  private sending: Subject<boolean> = new Subject();
+  public sending$: Observable<boolean> = this.sending.asObservable();
+
   constructor(private httpClient: HttpClient) {
   }
 
   public sendEmail(email: Email) {
+    this.sending.next(true);
     return this.httpClient.post("https://localhost:5001/api/v1/Email", email)
+      .pipe(tap(() => this.sending.next(false)))
   }
 }
